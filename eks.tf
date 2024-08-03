408747ecf33101c8df4c24da09ae5e5789fc8174
xnicho <hugh@hughnicholas.com>
2024-08-03 08:07:06 +0800

    building eks/sagemaker resources

--- eks.tf
+++ eks.tf
@@ -1,5 +1,5 @@
 resource "aws_eks_cluster" "main" {
-  name     = "anomaly-detection-cluster"
+  name     = "${local.name_prefix}-anomaly-detection-cluster"
   role_arn = aws_iam_role.eks_cluster_role.arn
 
   vpc_config {
@@ -11,3 +11,23 @@ resource "aws_eks_cluster" "main" {
     aws_iam_role_policy_attachment.eks_vpc_resource_controller_policy,
   ]
 }
+
+resource "aws_eks_node_group" "node_group" {
+  cluster_name    = aws_eks_cluster.main.name
+  node_group_name = "${local.name_prefix}-anomaly-detection-nodes"
+  node_role_arn   = aws_iam_role.eks_node_role.arn
+  subnet_ids      = aws_subnet.subnet[*].id
+
+  scaling_config {
+    desired_size = 2
+    max_size     = 3
+    min_size     = 1
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.eks_worker_node_policy,
+    aws_iam_role_policy_attachment.eks_cni_policy,
+    aws_iam_role_policy_attachment.eks_registry_policy,
+  ]
+}
+
--- sagemaker.tf
+++ sagemaker.tf
@@ -4,7 +4,7 @@ resource "aws_sagemaker_model" "anomaly_detection_model" {
   execution_role_arn = aws_iam_role.sagemaker_execution_role.arn
 
   primary_container {
-    image          = "123456789012.dkr.ecr.ap-southeast-2.amazonaws.com/anomaly-detection:latest"
+    image          = "128278156308.dkr.ecr.ap-southeast-2.amazonaws.com/anomaly-detection:latest"
     model_data_url = "s3://${aws_s3_bucket.sagemaker_bucket.bucket}/model.tar.gz"
   }
 }
